e8bdfaf98e9948e77fb2fba5ce5cd9bdf9ab2a4f
Nazieh <33094594+Nazieh@users.noreply.github.com>
2020-02-09 12:32:10 +0200

    Update train_script.py

--- train_script.py
+++ train_script.py
@@ -19,3 +19,96 @@ from datasets import camvid
 from datasets import joint_transforms
 import utils.imgs
 import utils.training as train_utils
+import os
+
+CAMVID_PATH = os.path.join("gdrive","My Drive","image_extraction","data","tiramisu")
+RESULTS_PATH = Path('.results/')
+WEIGHTS_PATH = Path('.weights/')
+RESULTS_PATH.mkdir(exist_ok=True)
+WEIGHTS_PATH.mkdir(exist_ok=True)
+batch_size = 2
+
+normalize = transforms.Normalize(mean=camvid.mean, std=camvid.std)
+train_joint_transformer = transforms.Compose([
+    #joint_transforms.JointRandomCrop(224), # commented for fine-tuning
+    joint_transforms.JointRandomHorizontalFlip()
+    ])
+train_dset = shirts.Shirts(CAMVID_PATH, 'train',
+      joint_transform=train_joint_transformer,
+      transform=transforms.Compose([
+          transforms.ToTensor(),
+          normalize,
+    ]))
+train_loader = torch.utils.data.DataLoader(
+    train_dset, batch_size=batch_size, shuffle=True)
+
+val_dset = shirts.Shirts(
+    CAMVID_PATH, 'val', joint_transform=None,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]))
+val_loader = torch.utils.data.DataLoader(
+    val_dset, batch_size=batch_size, shuffle=False)
+
+test_dset = shirts.Shirts(
+    CAMVID_PATH, 'test', joint_transform=None,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]))
+test_loader = torch.utils.data.DataLoader(
+    test_dset, batch_size=batch_size, shuffle=False)
+
+print("Train: %d" %len(train_loader.dataset.imgs))
+print("Val: %d" %len(val_loader.dataset.imgs))
+print("Test: %d" %len(test_loader.dataset.imgs))
+print("Classes: %d" % len(train_loader.dataset.classes))
+
+inputs, targets = next(iter(train_loader))
+print("Inputs: ", inputs.size())
+print("Targets: ", targets.size())
+
+utils.imgs.view_image(inputs[0])
+utils.imgs.view_annotated(targets[0])
+
+LR = 1e-4
+LR_DECAY = 0.005*LR
+DECAY_EVERY_N_EPOCHS = 1
+N_EPOCHS = 20
+torch.cuda.manual_seed(0)
+
+model = tiramisu.FCDenseNet67(n_classes=12).cuda()
+model.apply(train_utils.weights_init)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=LR, weight_decay=1e-4)
+criterion = nn.NLLLoss2d(weight=camvid.class_weight.cuda()).cuda()
+
+for epoch in range(1, N_EPOCHS+1):
+    since = time.time()
+
+    ### Train ###
+    trn_loss, trn_err = train_utils.train(
+        model, train_loader, optimizer, criterion, epoch)
+    print('Epoch {:d}\nTrain - Loss: {:.4f}, Acc: {:.4f}'.format(
+        epoch, trn_loss, 1-trn_err))    
+    time_elapsed = time.time() - since  
+    print('Train Time {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+
+    ### Test ###
+    val_loss, val_err = train_utils.test(model, val_loader, criterion, epoch)    
+    print('Val - Loss: {:.4f} | Acc: {:.4f}'.format(val_loss, 1-val_err))
+    time_elapsed = time.time() - since  
+    print('Total Time {:.0f}m {:.0f}s\n'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    
+    ### Checkpoint ###    
+    train_utils.save_weights(model, epoch, val_loss, val_err)
+
+    ### Adjust Lr ###
+    train_utils.adjust_learning_rate(LR, LR_DECAY, optimizer, 
+                                     epoch, DECAY_EVERY_N_EPOCHS)
+    
+train_utils.test(model, test_loader, criterion, epoch=1)
+train_utils.view_sample_predictions(model, test_loader, n=1)
+
