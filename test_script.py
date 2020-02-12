f784f712dbb34aae4fd466d279292d5f86f350a2
Nazieh <33094594+Nazieh@users.noreply.github.com>
2020-02-12 15:36:50 +0200

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,91 @@
+import time
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils import data
+
+from models import tiramisu
+from datasets import shirts
+from datasets import joint_transforms
+import utils.imgs
+import utils.training as train_utils
+import os
+
+
+CAMVID_PATH = os.path.join("gdrive","My Drive","image_extraction","data","tiramisu")
+RESULTS_PATH = Path('gdrive/My Drive/tiramisu/results/')
+WEIGHTS_PATH = Path('gdrive/My Drive/tiramisu/weights/')
+RESULTS_PATH.mkdir(exist_ok=True)
+WEIGHTS_PATH.mkdir(exist_ok=True)
+batch_size = 3
+
+mean = [0.41189489566336, 0.4251328133025, 0.4326707089857]
+std = [0.27413549931506, 0.28506257482912, 0.28284674400252]
+
+
+normalize = transforms.Normalize(mean=mean, std=std)
+train_joint_transformer = transforms.Compose([
+    joint_transforms.JointCenterCrop((512,224)), 
+    joint_transforms.JointRandomHorizontalFlip()
+    ])
+test_joint_transformer = transforms.Compose([
+    joint_transforms.JointCenterCrop((512,224))
+    ])
+
+train_dset = shirts.Shirts(CAMVID_PATH, 'train',
+      joint_transform=train_joint_transformer,
+      transform=transforms.Compose([
+          transforms.ToTensor(),
+          normalize,
+    ]))
+train_loader = torch.utils.data.DataLoader(
+    train_dset, batch_size=batch_size, shuffle=True)
+
+val_dset = shirts.Shirts(
+    CAMVID_PATH, 'val', joint_transform=test_joint_transformer,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]))
+val_loader = torch.utils.data.DataLoader(
+    val_dset, batch_size=batch_size, shuffle=False)
+
+test_dset = shirts.Shirts(
+    CAMVID_PATH, 'test', joint_transform=test_joint_transformer,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]))
+test_loader = torch.utils.data.DataLoader(
+    test_dset, batch_size=1, shuffle=False)
+
+print("Train: %d" %len(train_loader.dataset.imgs))
+print("Val: %d" %len(val_loader.dataset.imgs))
+print("Test: %d" %len(test_loader.dataset.imgs))
+print("Classes: %d" % len(train_loader.dataset.classes))
+
+inputs, targets = next(iter(train_loader))
+print("Inputs: ", inputs.size())
+print("Targets: ", targets.size())
+
+utils.imgs.view_image(inputs[0])
+utils.imgs.view_annotated(targets[0])
+
+LR = 1e-4
+LR_DECAY = 0.005*LR
+DECAY_EVERY_N_EPOCHS = 1
+N_EPOCHS = 20
+torch.cuda.manual_seed(0)
+
+#model = tiramisu.FCDenseNet67(n_classes=12).cuda()
+model = tiramisu.FCDenseNet00(n_classes=2).cuda()
+model.apply(train_utils.weights_init)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=LR, weight_decay=1e-4)
+#was criterion = nn.NLLLoss2d(weight=shirts.class_weight.cuda()).cuda()
+criterion = nn.NLLLoss2d().cuda()
