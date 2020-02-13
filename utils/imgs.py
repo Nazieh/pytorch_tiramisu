f5b1656eb12c20e41caa504e5f89298f970d0d1d
Nazieh <33094594+Nazieh@users.noreply.github.com>
2020-02-13 12:00:51 +0200

    Update imgs.py

--- utils/imgs.py
+++ utils/imgs.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+Void = [0,0,0]
+Print = [255,255,255]
+"""
 Sky = [128,128,128]
 Building = [128,0,0]
 Pole = [192,192,128]
@@ -13,12 +16,11 @@ Car = [64,0,128]
 Pedestrian = [64,64,0]
 Bicyclist = [0,128,192]
 Unlabelled = [0,0,0]
-
+"""
 DSET_MEAN = [0.41189489566336, 0.4251328133025, 0.4326707089857]
 DSET_STD = [0.27413549931506, 0.28506257482912, 0.28284674400252]
 
-label_colours = np.array([Sky, Building, Pole, Road, Pavement,
-      Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
+label_colours = np.array([Void,Print])
 
 
 def view_annotated(tensor, plot=True):
@@ -26,7 +28,7 @@ def view_annotated(tensor, plot=True):
     r = temp.copy()
     g = temp.copy()
     b = temp.copy()
-    for l in range(0,11):
+    for l in range(0,2):
         r[temp==l]=label_colours[l,0]
         g[temp==l]=label_colours[l,1]
         b[temp==l]=label_colours[l,2]
@@ -40,6 +42,23 @@ def view_annotated(tensor, plot=True):
         plt.show()
     else:
         return rgb
+      
+ def get_annotated(tensor):
+    temp = tensor.numpy()
+    r = temp.copy()
+    g = temp.copy()
+    b = temp.copy()
+    for l in range(0,2):
+        r[temp==l]=label_colours[l,0]
+        g[temp==l]=label_colours[l,1]
+        b[temp==l]=label_colours[l,2]
+
+    rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
+    rgb[:,:,0] = (r/255.0)#[:,:,0]
+    rgb[:,:,1] = (g/255.0)#[:,:,1]
+    rgb[:,:,2] = (b/255.0)#[:,:,2]
+    
+    return rgb
 
 def decode_image(tensor):
     inp = tensor.numpy().transpose((1, 2, 0))
